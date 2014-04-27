2c69a8d40db40472f4c7aecd8bf938d9da1a0cd5
Sebastian Bergmann <sb@sebastian-bergmann.de>
2014-04-27 11:29:09 +0200

    Cleanup

---
+++ README.md
@@ -0,0 +1,35 @@
+# PHPUnit Skeleton Generator
+
+`phpunit-skelgen` is a tool that can generate skeleton test classes from production code classes and vice versa.
+
+## Installation
+
+### PHP Archive (PHAR)
+
+The easiest way to obtain phpunit-skelgen is to download a [PHP Archive (PHAR)](http://php.net/phar) that has all required dependencies of phpunit-skelgen bundled in a single file:
+
+    wget https://phar.phpunit.de/phpunit-skelgen.phar
+    chmod +x phpunit-skelgen.phar
+    mv phpunit-skelgen.phar /usr/local/bin/phpunit-skelgen
+
+You can also immediately use the PHAR after you have downloaded it, of course:
+
+    wget https://phar.phpunit.de/phpunit-skelgen.phar
+    php phpunit-skelgen.phar
+
+### Composer
+
+Simply add a dependency on `phpunit/phpunit-skeleton-generator` to your project's `composer.json` file if you use [Composer](http://getcomposer.org/) to manage the dependencies of your project. Here is a minimal example of a `composer.json` file that just defines a development-time dependency on phpunit/phpunit-skeleton-generator:
+
+    {
+        "require-dev": {
+            "phpunit/phpunit-skeleton-generator": "*"
+        }
+    }
+
+For a system-wide installation via Composer, you can run:
+
+    composer global require 'phpunit/phpunit-skeleton-generator=*'
+
+Make sure you have `~/.composer/vendor/bin/` in your path.
+
--- src/AbstractGenerator.php
+++ src/AbstractGenerator.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * PHPUnit_SkeletonGenerator
+ * phpunit-skeleton-generator
  *
- * Copyright (c) 2012, Sebastian Bergmann <sebastian@phpunit.de>.
+ * Copyright (c) 2012-2014, Sebastian Bergmann <sebastian@phpunit.de>.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -34,152 +34,150 @@
  * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  *
- * @package    PHPUnit
- * @subpackage SkeletonGenerator
  * @author     Sebastian Bergmann <sebastian@phpunit.de>
- * @copyright  2012 Sebastian Bergmann <sebastian@phpunit.de>
+ * @copyright  2012-2014 Sebastian Bergmann <sebastian@phpunit.de>
  * @license    http://www.opensource.org/licenses/BSD-3-Clause  The BSD 3-Clause License
  * @link       http://www.phpunit.de/
  * @since      File available since Release 1.0.0
  */
 
-namespace SebastianBergmann\PHPUnit\SkeletonGenerator
+namespace SebastianBergmann\PHPUnit\SkeletonGenerator;
+
+/**
+ * Generator for skeletons.
+ *
+ * @author     Sebastian Bergmann <sebastian@phpunit.de>
+ * @copyright  2012-2014 Sebastian Bergmann <sebastian@phpunit.de>
+ * @license    http://www.opensource.org/licenses/BSD-3-Clause  The BSD 3-Clause License
+ * @link       http://www.phpunit.de/
+ * @since      Class available since Release 1.0.0
+ */
+abstract class AbstractGenerator
 {
     /**
-     * Generator for skeletons.
-     *
-     * @package    PHPUnit
-     * @subpackage SkeletonGenerator
-     * @author     Sebastian Bergmann <sebastian@phpunit.de>
-     * @copyright  2012 Sebastian Bergmann <sebastian@phpunit.de>
-     * @license    http://www.opensource.org/licenses/BSD-3-Clause  The BSD 3-Clause License
-     * @link       http://www.phpunit.de/
-     * @since      Class available since Release 1.0.0
+     * @var array
      */
-    abstract class AbstractGenerator
+    protected $inClassName;
+
+    /**
+     * @var string
+     */
+    protected $inSourceFile;
+
+    /**
+     * @var array
+     */
+    protected $outClassName;
+
+    /**
+     * @var string
+     */
+    protected $outSourceFile;
+
+    /**
+     * Constructor.
+     *
+     * @param string $inClassName
+     * @param string $inSourceFile
+     * @param string $outClassName
+     * @param string $outSourceFile
+     */
+    public function __construct($inClassName, $inSourceFile = '', $outClassName = '', $outSourceFile = '')
     {
-        /**
-         * @var array
-         */
-        protected $inClassName;
+        $this->inClassName = $this->parseFullyQualifiedClassName(
+            $inClassName
+        );
 
-        /**
-         * @var string
-         */
-        protected $inSourceFile;
+        $this->outClassName = $this->parseFullyQualifiedClassName(
+            $outClassName
+        );
 
-        /**
-         * @var array
-         */
-        protected $outClassName;
+        $this->inSourceFile = str_replace(
+            $this->inClassName['fullyQualifiedClassName'],
+            $this->inClassName['className'],
+            $inSourceFile
+        );
 
-        /**
-         * @var string
-         */
-        protected $outSourceFile;
-
-        /**
-         * Constructor.
-         *
-         * @param string $inClassName
-         * @param string $inSourceFile
-         * @param string $outClassName
-         * @param string $outSourceFile
-         */
-        public function __construct($inClassName, $inSourceFile = '', $outClassName = '', $outSourceFile = '')
-        {
-            $this->inClassName = $this->parseFullyQualifiedClassName(
-              $inClassName
-            );
-
-            $this->outClassName = $this->parseFullyQualifiedClassName(
-              $outClassName
-            );
-
-            $this->inSourceFile = str_replace(
-              $this->inClassName['fullyQualifiedClassName'],
-              $this->inClassName['className'],
-              $inSourceFile
-            );
-
-            $this->outSourceFile = str_replace(
-              $this->outClassName['fullyQualifiedClassName'],
-              $this->outClassName['className'],
-              $outSourceFile
-            );
-        }
-
-        /**
-         * @return string
-         */
-        public function getOutClassName()
-        {
-            return $this->outClassName['fullyQualifiedClassName'];
-        }
-
-        /**
-         * @return string
-         */
-        public function getOutSourceFile()
-        {
-            return $this->outSourceFile;
-        }
-
-        /**
-         * Generates the code and writes it to a source file.
-         *
-         * @param string $file
-         */
-        public function write($file = '')
-        {
-            if ($file == '') {
-                $file = $this->outSourceFile;
-            }
-
-            if ($fp = fopen($file, 'wt')) {
-                fwrite($fp, $this->generate());
-                fclose($fp);
-            }
-        }
-
-        /**
-         * @param  string $className
-         * @return array
-         */
-        protected function parseFullyQualifiedClassName($className)
-        {
-            $result = array(
-              'namespace'               => '',
-              'className'               => $className,
-              'fullyQualifiedClassName' => $className
-            );
-
-            if (strpos($className, '\\') !== FALSE) {
-                $tmp                 = explode('\\', $className);
-                $result['className'] = $tmp[count($tmp)-1];
-                $result['namespace'] = $this->arrayToName($tmp);
-            }
-
-            return $result;
-        }
-
-        /**
-         * @param  array $parts
-         * @return string
-         */
-        protected function arrayToName(array $parts)
-        {
-            $result = '';
-
-            if (count($parts) > 1) {
-                array_pop($parts);
-
-                $result = join('\\', $parts);
-            }
-
-            return $result;
-        }
-
-        abstract public function generate();
+        $this->outSourceFile = str_replace(
+            $this->outClassName['fullyQualifiedClassName'],
+            $this->outClassName['className'],
+            $outSourceFile
+        );
     }
+
+    /**
+     * @return string
+     */
+    public function getOutClassName()
+    {
+        return $this->outClassName['fullyQualifiedClassName'];
+    }
+
+    /**
+     * @return string
+     */
+    public function getOutSourceFile()
+    {
+        return $this->outSourceFile;
+    }
+
+    /**
+     * Generates the code and writes it to a source file.
+     *
+     * @param string $file
+     */
+    public function write($file = '')
+    {
+        if ($file == '') {
+            $file = $this->outSourceFile;
+        }
+
+        if ($fp = fopen($file, 'wt')) {
+            fwrite($fp, $this->generate());
+            fclose($fp);
+        }
+    }
+
+    /**
+     * @param  string $className
+     * @return array
+     */
+    protected function parseFullyQualifiedClassName($className)
+    {
+        $result = array(
+            'namespace'               => '',
+            'className'               => $className,
+            'fullyQualifiedClassName' => $className
+        );
+
+        if (strpos($className, '\\') !== false) {
+            $tmp                 = explode('\\', $className);
+            $result['className'] = $tmp[count($tmp)-1];
+            $result['namespace'] = $this->arrayToName($tmp);
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param  array $parts
+     * @return string
+     */
+    protected function arrayToName(array $parts)
+    {
+        $result = '';
+
+        if (count($parts) > 1) {
+            array_pop($parts);
+
+            $result = join('\\', $parts);
+        }
+
+        return $result;
+    }
+
+    /**
+     * @return string
+     */
+    abstract public function generate();
 }
