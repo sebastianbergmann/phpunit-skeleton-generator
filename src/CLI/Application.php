e199ec2d4db5512d3b728164ea395778d806839d
Sebastian Bergmann <sb@sebastian-bergmann.de>
2014-05-12 08:04:13 +0200

    Prepare release

--- src/CLI/Application.php
+++ src/CLI/Application.php
@@ -62,7 +62,7 @@ class Application extends AbstractApplication
 {
     public function __construct()
     {
-        $version = new Version('2.0', dirname(dirname(__DIR__)));
+        $version = new Version('2.0.0', dirname(dirname(__DIR__)));
         parent::__construct('phpunit-skelgen', $version->getVersion());
 
         $this->add(new GenerateClassCommand);
